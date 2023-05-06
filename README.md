da144e5e8ccadc7a75a8b485fe3381c187ac02d7
LLMTesting <LLMTesting@163.com>
2023-05-06 22:44:55 +0800

    upload

---
+++ README.md
@@ -0,0 +1,30 @@
+#Automatic Testing and Improvement for the Reasoning Ability of Large Language Models
+The repository is for the paepr "Automatic Testing and Improvement for the Reasoning Ability of Large Language Models", and TIRM is the abbreviation for our method.
+
+##Datasets
+According to the reasoning patterns we propose, we use atomic sentences to generate test cases, and we put the atomic sentences we wrote in the folder "data".
+
+##Testing Subject
+We test the most advanced LLM ChatGPT, and call the GPT-3.5 model through the API provided by openAI.
+
+##Our Approach
+Our method TIRM comprises the following three steps:
+
+(1)**Test generation.** We design six logical reasoning patterns to generate test cases, which follow the reasoning rules applied in boolean logic and are easy to understand for humans. We aim to employ generated test cases to detect whether LLMs have the essential reasoning ability.
+
+(2)**Answer Evaluation.** In this step, we compare the answer of the LLM to the logical reasoning test with the expected answer we design. When the similarity between the two is lower than a certain threshold, we judge that the LLM has a wrong answer.
+
+(3)**Model Prompting.** When logical reasoning errors are detected in the LLM, we hope to correct its error through reasonable prompting. For this reason, we design four different answer prompting methods and explore what kind of prompting methods are helpful to the LLM to correct the error.
+
+##Experiments
+We organize the codes in the repository according to RQs, and the codes of RQ1-3 are stored in folders with corresponding names, the code is named according to the name of the inference pattern:
+
+(1) RQ1 is the test result of a single round of question and answer, and the code is stored in the "RQ1" folder.
+
+(2) RQ2 is the experimental result of 10 questions and answers in one dialogue, and the code is stored in the "RQ2" folder.
+
+(3) RQ3 is the impact of the 4 model prompting methods on logical reasoning. This folder is divided into two parts according to the classification of the prompting methods, i.e., pre-prompting and post-prompting.
+
+#Snapshot
+
+We put the screenshots of the ChatGPT examples used in the paper in the snapshot folder, including the example in the motivation example and the two examples in RQ1.
---
+++ RQ3/pre_prompting/FDSR.py
@@ -0,0 +1,119 @@
+import openai
+from sentence_transformers import SentenceTransformer, util
+model = SentenceTransformer('paraphrase-mpnet-base-v2')
+
+with open("../../data/atomic_sentences_p.txt","r",encoding='utf-8') as f:
+    sentence1 = f.readlines()
+    sentence1 = [line.strip() for line in sentence1]
+
+with open("../../data/atomic_sentences_q.txt","r",encoding='utf-8') as f:
+    sentence2 = f.readlines()
+    sentence2 = [line.strip() for line in sentence2]
+
+pattern6_list = []
+ground_truth = []
+ground_truth2 = []
+
+for i in sentence1:
+    for j in sentence2:
+        pattern6 = "Suppose that \"" + i[:len(
+            i) - 1] + ", or " + j + "\" and \"" + i + "\" are true. What is the conclusion? (If there is a conclusion, you only response the conclusion without any analysis. Otherwise, you should only answer \"there is no conclusion\")"
+        pattern6_list.append(pattern6)
+        ground_truth2.append(i)
+        ground_truth.append('There is no conclusion.')
+
+
+
+openai.api_key = '' #The API key provided by openAI
+pattern1_results = []
+
+import time
+
+i = 0
+while i < len(pattern6_list):
+    pattern1_dict = {}
+    if i % 10 == 0:
+        messages = []
+        m = 1
+    message = {"role": "user", "content": pattern6_list[i]}
+    messages.append(message)
+    try:
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages,
+            temperature = 1.0
+        )
+    except:
+        k = i % 10
+        i = i - i % 10
+        print("warning!!!!")
+        pattern1_results = pattern1_results[:len(pattern1_results)-k]
+
+    else:
+        flag = 1
+
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content
+        response = {"role": "assistant", "content": result}
+        messages.append(response)
+        pattern1_dict['question'] = pattern6_list[i]
+        pattern1_dict['response'] = result
+
+        print('the {}-th turn:'.format(i), pattern1_dict)
+        time.sleep(10)
+        sent1 = result
+        sent2 = ground_truth[i]
+        sent3 = ground_truth2[i]
+
+        # Compute embedding for both lists
+        embeddings1 = model.encode(sent1, convert_to_tensor=True)
+        embeddings2 = model.encode(sent2, convert_to_tensor=True)
+        embeddings3 = model.encode(sent3, convert_to_tensor=True)
+
+        # Compute cosine-similarities
+        cosine_scores = util.cos_sim(embeddings1, embeddings2)
+        cosine_scores = cosine_scores.numpy()
+
+        cosine_scores2 = util.cos_sim(embeddings1, embeddings3)
+        cosine_scores2 = cosine_scores2.numpy()
+
+        if cosine_scores[0][0] < 0.5 and cosine_scores2[0][0] < 0.5 and m < 7:
+            try:
+                correct = 'The conclusion is wrong, the right one is that: ' + ground_truth[i]
+                # correct = 'The conclusion is wrong.'
+                # correct = 'The conclusion is wrong. Given \"p or q\" and \"p\" are true, where p and q represent statements, we can not infer whether q is true, so there is no conclusion.\n' + 'You should use this guideline to answer the following questions.'
+                message = {"role": "user", "content": correct}
+                messages.append(message)
+                response = openai.ChatCompletion.create(
+                    model='gpt-3.5-turbo',
+                    messages=messages,
+                    temperature=1.0
+                )
+            except:
+                flag = 0
+                k = i % 10
+                i = i - i % 10
+                print("warning!!!!")
+                pattern1_results = pattern1_results[:len(pattern1_results) - k]
+            else:
+                result = ''
+                for choice in response.choices:
+                    result += choice.message.content
+                response = {"role": "assistant", "content": result}
+                messages.append(response)
+                pattern1_dict['correct'] = correct
+                pattern1_dict['response_correct'] = result
+                print('the {}-th turn:'.format(i), pattern1_dict)
+                time.sleep(10)
+        if flag == 1:
+            pattern1_results.append(pattern1_dict)
+            i += 1
+        m += 1
+
+
+import json
+with open("FDSR.json","w",encoding = "utf-8") as f:
+    json_str = json.dumps(pattern1_results)
+    f.write(json_str)
+
